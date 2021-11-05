bf58a30062dc86750eda3de1e18a14ef3aaf2be3
Manuel Alférez <manuelalferezruiz@gmail.com>
2021-11-05 10:13:07 +0100

    Responsive design for hero

--- components/Hero.js
+++ components/Hero.js
@@ -1,16 +1,19 @@
 export default function Hero({ hero }) {
   return (
-    <div className="h-screen relative bg-gradient-to-l from-primary to-secondary">
-      <div className="flex justify-center pt-2">
+    <div className="h-screen relative overflow-hidden bg-gradient-to-r from-primary to-secondary">
+      <div className="flex justify-center pt-2 lg:justify-start lg:absolute lg:top-8 lg:left-24">
         <img src={hero.data.logo.url} />
         <h2 className="font-brand self-center	pl-2">{hero.data.brandname[0].text}</h2>
       </div>
-      <div className="absolute top-1/4 left-6">
+      <div className="absolute top-1/4 left-6 lg:w-2/6 lg:left-24 xl:w-3/12">
         <p className="font-brand text-tiny border-2 border-bcolor w-max text-bcolor px-1 justify-self-center rounded-md">
           {hero.data.announcement[0].text}
         </p>
-        <h1 className="text-3xl mt-6 text-white">{hero.data.title[0].text}</h1>
-        <p className="mt-12 pr-2 border-l-2 pl-6">{hero.data.description[0].text}</p>
+        <h1 className="text-3xl mt-6 text-white pr-2 lg:text-4xl">{hero.data.title[0].text}</h1>
+        <p className="mt-12 lg:mt-24 lg:pl-8 pr-2 border-l-2 pl-6">{hero.data.description[0].text}</p>
+      </div>
+      <div className="absolute hidden md:inline md:bottom-24 md:right-0 md:w-4/5 md:h-1/3 lg:top-60 lg:-right-12 lg:w-3/4 xl:w-max xl:top-12 xl:-right-8 2xl:top-40 2xl:right-4 object-scale-down">
+        <img src={hero.data.showcase.url} />
       </div>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -28,6 +28,22 @@ module.exports = {
       '6xl': '4rem',
       '7xl': '5rem',
     },
+    screens: {
+      sm: '640px',
+      // => @media (min-width: 640px) { ... }
+
+      md: '768px',
+      // => @media (min-width: 768px) { ... }
+
+      lg: '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      xl: '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1440px',
+      // => @media (min-width: 1536px) { ... }
+    },
   },
   variants: {
     extend: {
