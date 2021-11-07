37c38d27a2714f574c3b06c9e64ced7ecce90de4
Manuel Alférez <manuelalferezruiz@gmail.com>
2021-11-07 08:56:18 +0100

    Usecases created. No finished yet

--- components/Usecases.js
+++ components/Usecases.js
@@ -1,13 +1,13 @@
 export default function UseCases({ usecases }) {
   return (
-    <div className="bg-gradient-to-r from-primary to-secondary py-4 md:relative md:h-96 overflow-x-hidden overflow-y-overflow">
-      <h1 className="px-2 text-center text-xl py-12 md:w-2/4 md:mx-auto">
+    <div className="bg-gradient-to-r from-primary to-secondary py-4 md:relative md:h-screen overflow-visible overflow-x-hidden">
+      <h1 className="px-2 text-center text-xl py-12 md:w-2/4 md:mx-auto lg:text-4xl">
         Why PicLab is the perfect lorem ipsum for you.
       </h1>
-      <div className="md:flex md:flex-nowrap md:absolute z-40 md:left-12 md:top-42 md:overflow-visible">
+      <div className="md:flex md:flex-nowrap md:absolute md:left-12 md:top-96">
         {usecases.results.map((element) => {
           return (
-            <div className="bg-white text-black w-11/12 md:w-72 bg-white border-2 border-white p-8 m-2 rounded-xl font-thin relative">
+            <div className="bg-white text-black w-11/12 md:w-72 lg:w-96 bg-white border-2 border-white p-8 m-2 rounded-xl font-thin relative">
               <p className="text-xs">{element.data.category[0].text}</p>
               <h1 className="my-4 text-lg">{element.data.title[0].text}</h1>
               <p className="text-sm mb-48">{element.data.description[0].text}</p>
