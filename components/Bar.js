c9693d9be409c49c4f314afe0c52a8d93b5f975f
Manuel Alférez <manuelalferezruiz@gmail.com>
2021-11-07 15:30:26 +0100

    Bar for hero created

---
+++ components/Bar.js
@@ -0,0 +1,33 @@
+export default function Bar() {
+  return (
+    <div className="absolute text-xs w-11/12 bottom-12 lg:bottom-4 lg:left-10 xl:left-12 font-extralight text-black rounded-md hidden md:inline md:left-8 bg-white">
+      <div className="flex justify-between items-center p-2">
+        <div className="flex items-center px-2 border border-linecolor rounded-md">
+          <span classname="h-8 px-2">
+            <img src="https://ik.imagekit.io/manuelalferez/piclab/emailIcon_YilYQIaAc.png?updatedAt=1636279381029" />
+          </span>
+          <form className="p-2 flex items-center ">
+            <input
+              id="email"
+              classname="text-black"
+              style={{ outline: 'none' }}
+              placeholder="Enter email"
+              type="text"
+              autocomplete="email"
+            />
+            <button type="submit" className="font-extralight pl-8 border-l border-linecolor text-herocolor font-bold ">
+              Request Beta
+            </button>
+          </form>
+        </div>
+        <img src="https://ik.imagekit.io/manuelalferez/piclab/Group_1_5Mba_oQUB.png?updatedAt=1636279117468" />
+        <div className="flex w-20 justify-between mr-4">
+          <span classname="h-8">
+            <img src="https://ik.imagekit.io/manuelalferez/piclab/exitIcon_VB8H2Tvy4.png?updatedAt=1636279381024" />
+          </span>
+          <button className="font-extralight font-bold text-herocolor">Sign in</button>
+        </div>
+      </div>
+    </div>
+  );
+}
