f3f88458c9d8f777062dbaf18586defc12cdb2cf
Manuel Alférez <manuelalferezruiz@gmail.com>
2021-11-07 16:14:48 +0100

    Finishing bar

--- components/Bar.js
+++ components/Bar.js
@@ -1,12 +1,12 @@
 export default function Bar() {
   return (
-    <div className="absolute text-xs w-11/12 bottom-12 lg:bottom-4 lg:left-10 xl:left-12 font-extralight text-black rounded-md hidden md:inline md:left-8 bg-white">
-      <div className="flex justify-between items-center p-2">
-        <div className="flex items-center px-2 border border-linecolor rounded-md">
+    <div className="absolute text-xs w-11/12 bottom-4 lg:left-10 xl:left-12 font-extralight text-black rounded-md hidden md:inline md:left-8 bg-white">
+      <div className="flex justify-between items-center h-12 xl:h-20 relative">
+        <div className="absolute left-2 flex items-center px-2 border border-linecolor rounded-md">
           <span classname="h-8 px-2">
             <img src="https://ik.imagekit.io/manuelalferez/piclab/emailIcon_YilYQIaAc.png?updatedAt=1636279381029" />
           </span>
-          <form className="p-2 flex items-center ">
+          <form className="p-2 xl:py-4 flex items-center">
             <input
               id="email"
               classname="text-black"
@@ -20,8 +20,10 @@ export default function Bar() {
             </button>
           </form>
         </div>
-        <img src="https://ik.imagekit.io/manuelalferez/piclab/Group_1_5Mba_oQUB.png?updatedAt=1636279117468" />
-        <div className="flex w-20 justify-between mr-4">
+        <span className="absolute left-2/4">
+          <img src="https://ik.imagekit.io/manuelalferez/piclab/Group_1_5Mba_oQUB.png?updatedAt=1636279117468" />
+        </span>
+        <div className="flex w-20 justify-between mr-4 absolute right-0">
           <span classname="h-8">
             <img src="https://ik.imagekit.io/manuelalferez/piclab/exitIcon_VB8H2Tvy4.png?updatedAt=1636279381024" />
           </span>
