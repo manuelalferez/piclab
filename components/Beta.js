5afd2b943dc56d7123ee17790d3292af43d0bae3
Manuel Alférez <manuelalferezruiz@gmail.com>
2021-11-08 10:31:01 +0100

    Organazing styles

--- components/Beta.js
+++ components/Beta.js
@@ -2,15 +2,15 @@ export default function Beta({ beta }) {
   return (
     <div className="h-screen relative bg-betacolor flex flex-col justify-center text-xs">
       <h1 className="px-2 text-center text-xl py-12 md:w-2/4 md:mx-auto lg:text-4xl">{beta.data.title[0].text}</h1>
-      <div className="sticky bg-white z-10 text-black md:w-6/12 lg:w-4/12 md:mx-auto md:p-2 md:px-4 flex items-center mx-4 px-2 border border-linecolor rounded-md">
+      <div className="sticky bg-white z-10 text-black flex items-center mx-4 px-2 border border-linecolor rounded-md md:w-6/12 md:mx-auto md:p-2 md:px-4 lg:w-4/12">
         <span classname="h-8 px-2">
           <img src="https://ik.imagekit.io/manuelalferez/piclab/emailIcon_YilYQIaAc.png?updatedAt=1636279381029" />
         </span>
-        <form className="p-2 xl:py-4 flex items-center">
+        <form className="p-2 flex items-center xl:py-4">
           <input id="email" style={{ outline: 'none' }} placeholder="Enter email" type="text" autocomplete="email" />
           <button
             type="submit"
-            className="absolute right-2 md:right-4 font-extralight pl-8 border-l border-linecolor text-herocolor font-bold "
+            className="absolute right-2 font-extralight pl-8 border-l border-linecolor text-herocolor font-bold md:right-4"
           >
             Request Beta
           </button>
--- components/Usecases.js
+++ components/Usecases.js
@@ -1,13 +1,13 @@
 export default function UseCases({ usecases }) {
   return (
-    <div className="bg-gradient-to-r from-primary to-secondary py-4 md:relative md:h-screen overflow-visible overflow-x-hidden">
+    <div className="bg-gradient-to-r from-primary to-secondary py-4 overflow-visible overflow-x-hidden md:relative md:h-screen">
       <h1 className="px-2 text-center text-xl py-12 md:w-2/4 md:mx-auto lg:text-4xl">
         Why PicLab is the perfect lorem ipsum for you.
       </h1>
       <div className="md:flex md:flex-nowrap md:absolute md:left-12 md:top-96">
         {usecases.results.map((element) => {
           return (
-            <div className="bg-white text-black w-11/12 md:w-72 lg:w-96 bg-white border-2 border-white p-8 m-2 rounded-xl font-thin relative">
+            <div className="relative bg-white text-black w-11/12 md:w-72 lg:w-96 bg-white border-2 border-white p-8 m-2 rounded-xl font-thin">
               <p className="text-xs">{element.data.category[0].text}</p>
               <h1 className="my-4 text-lg">{element.data.title[0].text}</h1>
               <p className="text-sm mb-48">{element.data.description[0].text}</p>
